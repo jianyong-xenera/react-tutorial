6e48dfd3678d230603b3d14b56c82481779544cb
P0155832 <hongjian.chen@pactera.com>
2020-12-16 16:37:34 +0800

    hongjian.chen

--- 3. 第一个App/src/App.js
+++ 3. 第一个App/src/App.js
@@ -17,6 +17,16 @@ class App extends Component {
         });
     }
 
+    updateCharacter = index => {
+        const { characters } = this.state;
+
+        this.setState({
+            characters: characters.filter((character, i) => {
+                return i !== index;
+            })
+        });
+    }
+
     handleSubmit = character => {
         this.setState({characters: [...this.state.characters, character]});
     }
@@ -31,6 +41,7 @@ class App extends Component {
                 <Table
                     characterData={characters}
                     removeCharacter={this.removeCharacter}
+                    updateCharacter={this.updateCharacter}
                 />
                 <h3>Add New</h3>
                 <Form handleSubmit={this.handleSubmit} />
