fd659421885f24a41bdcec0daf8b0d1ea111d59b
P0155832 <hongjian.chen@pactera.com>
2020-12-25 14:14:32 +0800

    第三次作业

---
+++ 第3课-React进阶/作业/useFriendStatus.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from "react";
+
+function useFriendStatus(friendID) {
+  const [isOnline, setIsOnline] = useState(null);
+
+  function handleStatusChange(status) {
+    setIsOnline(status.isOnline);
+  }
+
+  useEffect(() => {
+    ChatAPI.subscribeToFriendStatus(friendID, handleStatusChange);
+    return () => {
+      ChatAPI.unsubscribeFromFriendStatus(friendID, handleStatusChange);
+    };
+  });
+
+  return isOnline;
+}
+
+function higherFunction(WrappedComponent, friendID) {
+  return class extends React.Component {
+    constructor(props) {
+      super(props);
+      this.handleChange = this.handleChange.bind(this);
+      this.state = {
+        data: friendID,
+      };
+    }
+
+    subscribe() {
+      DataSource.addChangeListener(this.handleChange);
+    }
+
+    cancel() {
+      DataSource.removeChangeListener(this.handleChange);
+    }
+
+    handleChange() {
+      this.setState({
+        data: friendID,
+      });
+    }
+
+    render() {
+      // ... 并使用新数据渲染被包装的组件!
+      // 请注意，我们可能还会传递其他属性
+      return <WrappedComponent data={this.state.data} {...this.props} />;
+    }
+  };
+}
+
+
+
